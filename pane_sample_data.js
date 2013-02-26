0c9312bdcdf1fa635a2096379580d289bbc98f1d
isheu <isheu07@gmail.com>
2013-02-26 09:13:13 -0800

    More style guidelines, experimenting

---
+++ pane_sample_data.js
@@ -0,0 +1,24 @@
+var dwarves = [
+      {"name":"Kili", "bow": 100, "axe": 25, "fire_breath":0},
+      {"name":"Smaug", "bow":0, "axe":2, "fire_breath":100},
+      {"name":"Gollum", "bow":50, "axe":2, "fire_breath":0},
+      {"name":"Gandalf", "bow":20, "axe":2, "fire_breath":76},
+      {"name":"Legolas", "bow":100, "axe":60, "fire_breath":0},
+      {"name":"Aragorn", "bow":89, "axe":75, "fire_breath":40},
+      {"name":"Merry", "bow":30, "axe":23, "fire_breath":20},
+      {"name":"Pippin", "bow":20, "axe":24, "fire_breath":0},
+      {"name":"Ent", "bow":0, "axe":2, "fire_breath":0}
+      ];
+
+var dates_data = [
+      {"date":"03JAN2008", "price":47282},
+      {"date":"12FEB2009", "price":50894},
+      {"date":"13MAR2009", "price":45687},
+      {"date":"03APR2009", "price":36578},
+      {"date":"09MAY2009", "price":48752},
+      {"date":"14JUN2009", "price":58692},
+      {"date":"03DEC2009", "price":47213},
+      {"date":"27FEB2010", "price":46127},
+      {"date":"21APR2010", "price":50652},
+      {"date":"19AUG2010", "price":51236}
+      ];
