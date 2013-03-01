e8886aa444d05edaa42e739dacbd7febe73e9d22
unknown <isheu@isheu7.win.sphereinstitute.org>
2013-02-28 17:10:57 -0800

    Work computer commit - added re-usable transitions

--- pane_helper.js
+++ pane_helper.js
@@ -1,6 +1,7 @@
 function getDate(d) {
    return new Date(d);
 }
+
 function update_cursor_line() {
    d3.select("g#cursor_line_to_axis")
       .style("display", "inline");         
@@ -26,7 +27,63 @@ function update_cursor_line() {
    d3.select("g#cursor_line_to_axis")
       .attr("transform", function() { return "translate(" + MousePosition[0] + "," + (y_plot_displace) + ")";});
    }
+
 function nodisplay_cursor_line() {
    d3.select("g#cursor_line_to_axis")
       .style("display", "none");
    }
+
+
+
+function colored_transition() {
+   var b_color = "rgba(128,0,0,0.1)";
+   var t_color = "rgba(128,0,0,0)";
+   
+   function transition(g) {
+      d3.select(this)
+         .style("background-color", b_color)
+         .transition()
+         .duration(250)
+         .style("background-color", t_color);
+      }
+   transition.base_color = function(value) {
+      b_color = value;
+      return transition;
+   }
+   transition.transition_color = function(value) {
+      t_color = value;
+      return transition;
+   }
+   return transition;
+}
+
+
+// Getter-setter methods, for method-chaining. Modular organization of code.
+function gen_time_scatterplot(metric_X, metric_Y) {
+   var width = 550;        
+   var height = 450;   
+   var date_st = new Date(2006,12,31);
+   var date_end = new Date(2013,1,1);
+   var y_st = 0;
+   var y_end = 15000;
+   
+   var x_domain = [date_st, date_end];
+   var y_domain = [y_st, y_end];   
+   var x_time_scale = d3.time.scale()
+      .domain(x_domain)
+      .range([0, width]);
+   var y_scale = d3.scale.linear()
+      .domain(y_domain)
+      .range([0, height]);
+
+   function draw_time_Xy_plot() {
+      var xAxis = d3.svg.axis().scale(x_time_scale)
+         .orient("bottom")
+         .ticks(d3.time.months, 6).tickSubdivide(5).tickSize(6,3,0)
+         .tickFormat(d3.time.format("%b %y"));
+      var yAxis = d3.svg.axis().scale(y_scale)
+         .orient("left")
+         .ticks(6).tickSubdivide(4).tickSize(6,3,0)
+         .tickFormat(function(d) { return "$" + price_formatter(d); } );
+      }
+}
