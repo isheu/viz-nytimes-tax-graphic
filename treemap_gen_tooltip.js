51bac1398623b251b2b3e6436c1f2f3ee289d066
unknown <isheu@isheu7.win.sphereinstitute.org>
2013-07-16 19:04:23 -0700

    separate graphic and setup

--- treemap_gen_tooltip.js
+++ treemap_gen_tooltip.js
@@ -9,24 +9,3 @@ function tooltip(ttip_data, height, width) {
       }
    return draw_tooltip
 }
-
-            /*
-            d3.selectAll(".f_circle").data().forEach(function(d, i) { 
-                     filt_clustered_pos[i] = {"radius": d.radius, "x": d.x, "y": d.y, "t_value": d.t_value, "name": d.name, "sector": d.sector, "tax_rate": d.tax_rate}
-                  });         
-            d3.selectAll(".f_circle").remove()
-            d3.selectAll(".n_circle")
-               .data(filt_clustered_pos, function(d) { return d.name; })
-               .attr("id", function(d) { return d.name })
-               .transition().duration(1200)
-               .delay(function(d, i) { return (d.tax_rate / 100) * 1200; })
-               .attr("cx", function(d) { return d.x; })
-               .attr("cy", function(d) { return d.y; })
-         var filt_clustered_pos = [];
-         setTimeout(function(g) {
-            console.log("try now")
-            d3.selectAll(".n_circle")
-               .on("mouseover", function() { d3.select(this).attr("stroke", "black"); })
-               .on("mouseout", function() { d3.select(this).attr("stroke", "white"); });
-         }, 18000);
-            */
